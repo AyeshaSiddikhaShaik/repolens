5ae41084ac84db8d70ae8205c38c0635836df873
AyeshaSiddikhaShaik <ayeshasiddikhashaik11@gmail.com>
2026-03-12 20:47:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -26,34 +26,3 @@ elif data["stars"] > 10000:
     print("This project has strong community support.")
 else:
     print("This is a smaller or growing repository.")
-# import requests
-
-# repo = input("Enter GitHub repository (owner/repo): ")
-
-# url = f"https://api.github.com/repos/{repo}"
-
-# response = requests.get(url)
-
-# data = response.json()
-# if response.status_code != 200:
-#     print("Repository not found. Please check owner/repo name.")
-#     exit()
-
-# print("\nRepository Information")
-# print("----------------------")
-# print("Name:", data["name"])
-# print("Owner:", data["owner"]["login"])
-# print("Stars:", data["stargazers_count"])
-# print("Forks:", data["forks_count"])
-# print("Language:", data["language"])
-# print("Open Issues:", data["open_issues_count"])
-
-# print("\nProject Insight")
-# print("----------------")
-
-# if data["stargazers_count"] > 50000:
-#     print("This is a very popular open-source project.")
-# elif data["stargazers_count"] > 10000:
-#     print("This project has strong community support.")
-# else:
-#     print("This is a smaller or growing repository.")
